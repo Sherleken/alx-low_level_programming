c21fb865a63c86dbbd3bc36a28805ec8d3aba731
= <=>
2022-10-18 13:33:37 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: this is the first number
+ * @b: this is the second number
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
