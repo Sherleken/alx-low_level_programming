81e6d0155316ca35803c06ea4fdc9cc27559c9b5
= <=>
2022-10-18 12:57:07 -0700

    Task 6

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - this checks if the character is lowercase
- * @c - this is the character
+ * @c: this is the character
  * Return: 1 if the character is lowercase and 0 if it is not
  */
 int _islower(int c)
