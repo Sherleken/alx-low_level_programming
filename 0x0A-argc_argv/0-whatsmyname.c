6d4daac04c04aa8504b3892bbb7c517fd7edc9f3
= <=>
2022-11-07 12:44:04 -0800

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of program
+ * @argc: number of command line arguments
+ * @argv: name of array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+
+}
