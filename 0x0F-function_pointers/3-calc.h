ebe065935ccd2a25cd2f7ba8e8d8e03129e3197f
= <=>
2022-11-19 03:07:18 -0800

    Task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,8 +1,10 @@
-#ifndef _CALC_H_
-#define _CALC_H_
+#ifndef CALCFUNC
+#define CALCFUNC
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
 /**
  * struct op - struct op
  * @op: operator
@@ -14,11 +16,11 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
+int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
 
 #endif
