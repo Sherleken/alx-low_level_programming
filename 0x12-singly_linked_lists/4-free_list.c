324987040242163a7b8391e789c3ca36f810dd1a
Sherleken <Sherleken>
2022-11-28 20:32:11 +0000

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: list_t list to be freed
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	free_list(head->next);
+	free(head);
+}
+
